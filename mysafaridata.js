6d9bfba619d9b2ea1101beb40f8493230b748aec
sirrah23 <hseth93@gmail.com>
2017-08-18 01:12:08 -0400

    Repeatedly click more before loading books
    The SafariBooks queue page has a more button. This
    bookmarklet will now repeatedly click the more button
    until there are no more books left in the queue.
    
    After that, same as before, the book data will be
    turned into a JSON string and stored on the window.

--- mysafaridata.js
+++ mysafaridata.js
@@ -1,17 +1,30 @@
 javascript:(function(){ 
-    //TODO: Add a loop to click the "more" button repeatedly...
-    const books = document.getElementsByClassName("info");
-    const data = [];
-    let title, pctread_node, pctread;
-    for(let i = 0; i < books.length; i++){
-        title = books[i].querySelector("div.info > div.main > a.title-block > span.chapter-title").innerHTML;
-        pctread_node = books[i].querySelector("div.main > div.progress span.gridtc div.time");
-        if (pctread_node){
-            pctread = pctread_node["style"].width ;
-        } else {
-            pctread = "0%";
+    const waittime = 5000;
+    let morebutton;
+    function bookdatatojson(){
+        const books = document.getElementsByClassName("info");
+        const data = [];
+        let title, pctread_node, pctread;
+        for(let i = 0; i < books.length; i++){
+            title = books[i].querySelector("div.info > div.main > a.title-block > span.chapter-title").innerHTML;
+            pctread_node = books[i].querySelector("div.main > div.progress span.gridtc div.time");
+            if (pctread_node){
+                pctread = pctread_node["style"].width ;
+            } else {
+                pctread = "0%";
+            }
+            data.push({title, pctread});
         }
-        data.push({title, pctread});
+        window.globalmybookdata = JSON.stringify(data);
     }
-    window.globalmybookdata = JSON.stringify(data);
+    function loadallbooks(){
+        morebutton = document.getElementsByClassName('showmore');
+        if(morebutton.length > 0){
+            morebutton[0].click();
+            setTimeout(loadallbooks,waittime);
+        } else{
+            bookdatatojson();
+        }
+    }
+    loadallbooks();
 })()
